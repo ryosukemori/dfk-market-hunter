3243d29320af446a26abc83508761517235ae314
Ryosuke Mori <ryosuke.mori@nickellab.net>
2021-11-21 03:43:09 +0900

    add wss

--- src/wallet/index.ts
+++ src/wallet/index.ts
@@ -1,14 +1,18 @@
 import { ethers } from 'ethers'
 
+const providerMode = process.env.PROVIDER || 'http'
 const rpc = process.env.RPC || ''
+const rpc_wss = process.env.RPC_WSS || ''
 const pk = process.env.PK || ''
 
 export const getWallet = () => {
   const provider = getProvider()
-  const wallet = new ethers.Wallet(pk, provider)
-  return wallet
+  return new ethers.Wallet(pk, provider)
 }
 
 export const getProvider = () => {
+  if (providerMode === 'wss') {
+    return new ethers.providers.WebSocketProvider(rpc_wss)
+  }
   return new ethers.providers.JsonRpcProvider(rpc)
 }
